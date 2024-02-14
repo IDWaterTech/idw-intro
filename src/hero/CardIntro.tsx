795dbfb1a3a1cca22892a8912ac3b58826708657
Rémi Weng <ixartz@users.noreply.github.com>
2024-02-15 00:03:22 +0100

    add CardIntro元件

---
+++ src/hero/CardIntro.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+interface CardProps {
+  imageSrc: string;
+  title: string;
+  description: string;
+  rightAnglesPosit?: number; // 直角保留 全部0 左上1 右上2 左下3 右下4
+}
+
+const CardIntro: React.FC<CardProps> = ({
+  imageSrc,
+  title,
+  description,
+  rightAnglesPosit = 0,
+}) => {
+  let posit = '';
+  switch (rightAnglesPosit) {
+    case 0: // 全部都直角
+      posit = '';
+      break;
+    case 1: // 左上是直角，其他圓角
+      posit = 'rounded-b-3xl rounded-tr-3xl';
+      break;
+    case 2: // 右上是直角，其他圓角
+      posit = 'rounded-b-3xl rounded-tl-3xl';
+      break;
+    case 3: // 左下是直角，其他圓角
+      posit = 'rounded-t-3xl rounded-br-3xl';
+      break;
+    case 4: // 右下是直角，其他圓角
+      posit = 'rounded-t-3xl rounded-bl-3xl';
+      break;
+    case 5: // 全部圓角
+      posit = 'rounded-3xl';
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className={`grid grid-cols-12 gap-x-2 ${posit} bg-white shadow-2xl`}>
+      <div className="col-span-12 rounded-t-3xl">
+        <img
+          className="h-[200px] w-full rounded-t-3xl object-cover p-8"
+          src={imageSrc}
+          alt={title}
+        />
+      </div>
+      <div className="col-span-12 mx-2 p-4">
+        <h3 className="mb-2 text-xl font-bold">{title}</h3>
+        <span
+          className="text-gray-700"
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></span>
+      </div>
+    </div>
+  );
+};
+
+export default CardIntro;
---
+++ src/templates/Hero.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+
+import CardIntro from '@/hero/CardIntro';
+
+import { Background } from '../background/Background';
+import { Button } from '../button/Button';
+import { HeroOneButton } from '../hero/HeroOneButton';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
+
+const Hero = () => (
+  <Background color="bg-gray-600">
+    <Section yPadding="py-6">
+      <NavbarTwoColumns logo={<Logo xl />}>
+        <li>
+          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+            GitHub
+          </Link>
+        </li>
+        <li>
+          <Link href="/">Sign in</Link>
+        </li>
+      </NavbarTwoColumns>
+    </Section>
+
+    <Section yPadding="pt-20 pb-32">
+      <HeroOneButton
+        title={
+          <>
+            {'The modern landing page for\n'}
+            <span className="text-primary-500">React developers</span>
+          </>
+        }
+        description="The easiest way to build a React landing page in seconds."
+        button={
+          <Link href="https://creativedesignsguru.com/category/nextjs/">
+            <Button xl>Download Your Free Theme</Button>
+          </Link>
+        }
+      />
+    </Section>
+    <Section>
+      <CardIntro
+        imageSrc="https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=2008&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="即時數據"
+        description="隨時檢視最新數據即時掌握養殖池，降低死亡率<br/><ul style='list-style-type :disc'><li>檢視各池養殖資訊</li><li>即時檢視各池水質狀態</li></ul>"
+        rightAnglesPosit={5}
+      />
+    </Section>
+  </Background>
+);
+
+export { Hero };
