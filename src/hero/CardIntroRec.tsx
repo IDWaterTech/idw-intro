dd235be0e08ef73e75acdd3139576a08c55d4b81
wei10cool <x215033@yahoo.com.tw>
2024-08-12 10:03:38 +0800

    改預設圓角

---
+++ src/hero/CardIntroRec.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+interface CardProps {
+  imageSrc: string;
+  title: string;
+  description: string;
+  rightAnglesPosit?: number; // 直角保留 全部0 左上1 右上2 左下3 右下4 全園5
+}
+
+const CardIntro: React.FC<CardProps> = ({
+  imageSrc,
+  title,
+  description,
+  rightAnglesPosit = 5,
+}) => {
+  let posit = '';
+  switch (rightAnglesPosit) {
+    case 0: // 全部都直角
+      posit = '';
+      break;
+    case 1: // 左上是直角，其他圓角
+      posit = 'rounded-b-3xl rounded-tr-3xl';
+      break;
+    case 2: // 右上是直角，其他圓角
+      posit = 'rounded-b-3xl rounded-tl-3xl';
+      break;
+    case 3: // 左下是直角，其他圓角
+      posit = 'rounded-t-3xl rounded-br-3xl';
+      break;
+    case 4: // 右下是直角，其他圓角
+      posit = 'rounded-t-3xl rounded-bl-3xl';
+      break;
+    case 5: // 全部圓角
+      posit = 'rounded-3xl';
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className={`grid grid-cols-12 p-4 ${posit} bg-white shadow-2xl`}>
+      <div className="col-span-4 flex items-center justify-center">
+        <div className="size-24 overflow-hidden rounded-full">
+          <img className="size-full object-cover" src={imageSrc} alt={title} />
+        </div>
+      </div>
+      <div className="col-span-8">
+        <h3 className="mb-2 text-xl font-bold">{title}</h3>
+        <span
+          className="text-gray-700"
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></span>
+      </div>
+    </div>
+  );
+};
+
+export default CardIntro;
