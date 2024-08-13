cf21e6481e148bdc6d0d9d1ef332b27c92cf94dd
wei10cool <x215033@yahoo.com.tw>
2024-08-13 16:45:50 +0800

    先刪

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,7 +1,7 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { AIot } from './AIot';
-import { AquaCultural } from './AquaCultural';
+// import { AIot } from './AIot';
+// import { AquaCultural } from './AquaCultural';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
@@ -10,8 +10,8 @@ const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
-    <AquaCultural />
-    <AIot />
+    {/* <AquaCultural />
+    <AIot /> */}
     <Banner />
     <Footer />
   </div>
