078d294ddd91a6b8baffd3e0588a4931ff4f3968
wei10cool <x215033@yahoo.com.tw>
2024-08-14 17:05:22 +0800

    改文字

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: '艾滴科技',
+  title: '艾滴科技-養殖專家系統，值得您信賴的智慧養殖好幫手',
+  description:
+    '即時、快速的一站式養殖服務、省時省力又省錢、AIOT的AI輔助有效監測、多樣功能的精準掌握數據',
+  locale: 'zh-tw',
 };
