c99b0ef5795a72f879dc8b90e8a4bd699964a1a5
wei10cool <x215033@yahoo.com.tw>
2024-08-14 12:01:18 +0800

    hero加入動態效果

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { Background } from '../background/Background';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
@@ -41,18 +43,23 @@ const functionsImage = [
     url: 'calendar.png',
   },
 ];
-const Hero = () => (
-  <Background>
-    <Section yPadding="py-0 px-0" navbar="true">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        {navbarList.map((nav) => {
-          return (
-            <li className={styles.listItem} key={nav.id}>
-              {nav.name}
-            </li>
-          );
-        })}
-        {/* <li>
+const Hero = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setIsVisible(true); // 当组件挂载后，触发动画效果
+  }, []);
+  return (
+    <Background>
+      <Section yPadding="py-0 px-0" navbar="true">
+        <NavbarTwoColumns logo={<Logo xl />}>
+          {navbarList.map((nav) => {
+            return (
+              <li className={styles.listItem} key={nav.id}>
+                {nav.name}
+              </li>
+            );
+          })}
+          {/* <li>
           <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
             GitHub
           </Link>
@@ -60,32 +67,38 @@ const Hero = () => (
         <li>
           <Link href="/">Sign in</Link>
         </li> */}
-      </NavbarTwoColumns>
-    </Section>
+        </NavbarTwoColumns>
+      </Section>
 
-    <Section yPadding="pt-8 pb-0">
-      <HeroOneButton
-        title={
-          <>
-            {'ID Water 專家系統\n'}
-            <span className="hero-subtitle">打造可信賴的AI智慧養殖系統</span>
-          </>
-        }
-      />
-    </Section>
-    <Section yPadding="pt-16 pb-16">
-      <div className={styles.imageGroup}>
-        {functionsImage.map((image, i) => {
-          return (
-            <div className={styles.imageBox} key={`image_${i}`}>
-              <img src={`/assets/images/${image.url}`} alt={image.name} />
-              <span>{image.name}</span>
-            </div>
-          );
-        })}
-      </div>
-    </Section>
-  </Background>
-);
+      <Section yPadding="pt-8 pb-0">
+        <HeroOneButton
+          title={
+            <>
+              {'ID Water 專家系統\n'}
+              <span className="hero-subtitle">打造可信賴的AI智慧養殖系統</span>
+            </>
+          }
+        />
+      </Section>
+      <Section yPadding="pt-16 pb-16">
+        <div className={styles.imageGroup}>
+          {functionsImage.map((image, i) => {
+            return (
+              <div
+                className={`${styles.imageBox} ${styles['card-animation']} ${
+                  isVisible ? styles['card-animation-enter'] : ''
+                } ${styles['card-hover']}`}
+                key={`image_${i}`}
+              >
+                <img src={`/assets/images/${image.url}`} alt={image.name} />
+                <span>{image.name}</span>
+              </div>
+            );
+          })}
+        </div>
+      </Section>
+    </Background>
+  );
+};
 
 export { Hero };
