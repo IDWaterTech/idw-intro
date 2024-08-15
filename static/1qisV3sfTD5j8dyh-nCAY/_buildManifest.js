ff3348e0665753d81f6f0f78fda4a88ec031aafc
wei10cool <x215033@yahoo.com.tw>
2024-08-15 16:21:31 +0800

    Updates

---
+++ static/1qisV3sfTD5j8dyh-nCAY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/815-ac50f42d051b43d2.js","static/css/e12264a4b49781ae.css","static/chunks/pages/index-c0392d914b357b15.js"],"/_error":["static/chunks/pages/_error-b9e1de59d6b43160.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
