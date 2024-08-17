1a8714e3dfb080899e2c3ac6703a0fee0c7c4931
wei10cool <x215033@yahoo.com.tw>
2024-08-17 15:21:21 +0800

    Updates

---
+++ _next/static/QMaPE4-xzgwKosin-fzFH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/498-6034390c28a71ce9.js","static/css/e12264a4b49781ae.css","static/chunks/pages/index-7e4a31ab7af23dcc.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
