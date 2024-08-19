3fb5e077fd9221812a2949986df5839d7022c998
wei10cool <x215033@yahoo.com.tw>
2024-08-19 08:43:02 +0800

    註解佈署設定

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ module.exports = withBundleAnalyzer({
   output: 'export',
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '/idw-intro', // 加上去在，dev開發模式會有問題
-  assetPrefix: '/idw-intro/', // 加上去在，dev開發模式會有問題
+  // basePath: '/idw-intro', // 加上去在，dev開發模式會有問題
+  // assetPrefix: '/idw-intro/', // 加上去在，dev開發模式會有問題
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
