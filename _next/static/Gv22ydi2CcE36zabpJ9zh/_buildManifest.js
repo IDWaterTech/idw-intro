2ad16399efdd472d57f3d595888ea0bc7fa5ca0e
hsushihya <shihya.hsu@idwater.com.tw>
2024-08-19 17:20:25 +0800

    Updates

--- _next/static/Gv22ydi2CcE36zabpJ9zh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/927-68fcdce1ef2ca897.js","static/css/013c671ac73f33a6.css","static/chunks/pages/index-bf8b15d8908ef383.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
