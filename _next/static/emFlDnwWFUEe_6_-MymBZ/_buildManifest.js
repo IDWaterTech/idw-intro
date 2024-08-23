2fb858310ff80bb7e4135dc27d47983927aa1ab3
wei10cool <x215033@yahoo.com.tw>
2024-08-23 11:33:37 +0800

    Updates

--- _next/static/emFlDnwWFUEe_6_-MymBZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/498-6034390c28a71ce9.js","static/css/c4bff0a5564dec98.css","static/chunks/pages/index-28b0e1dcd2b4d3df.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
