1b5635e2ebd736c668e1fdf04fce1c9c9549a24a
wei10cool <x215033@yahoo.com.tw>
2024-09-02 15:25:38 +0800

    調整圖片過大使用cover方式

--- src/background/Background.tsx
+++ src/background/Background.tsx
@@ -12,7 +12,7 @@ const Background = (props: IBackgroundProps) => (
       {`
           .bg {
                 background-image:url(./assets/images/hero/hero-bg.png);
-                background-size: auto 100vh;
+                background-size: cover;
                 background-position: center;
                 background-repeat: no-repeat;
               }
@@ -20,6 +20,9 @@ const Background = (props: IBackgroundProps) => (
         `}
     </style>
     <div
+      // 紅樹林圖片
+      // https://images.unsplash.com/photo-1623719647321-188ae37d7133?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=1024
+      // https://images.unsplash.com/photo-1520333764090-778d6dc95ec3?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=1920
       // eslint-disable-next-line no-nested-ternary, eqeqeq
       className={`${props.color ? (props.color == 'bg-transparent' ? 'nav-bg absolute left-0 top-0 w-full' : `${props.color} relative`) : 'bg relative'}`}
     >
